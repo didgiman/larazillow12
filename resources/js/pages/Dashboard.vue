b5af74f6c84717f3d19a41f7a259d2ce6ff2af29
Wim Lemmens <wim.lemmens@gmail.com>
2025-07-07 08:32:27 +0200

    Update reka-ui to version 2.3.2, implement accordion and switch components, and enhance Dashboard with new UI elements

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -3,6 +3,8 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import Switch from '@/components/ui/switch/Switch.vue';
+import EmailNotifications from '@/components/EmailNotifications.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -18,11 +20,13 @@ const breadcrumbs: BreadcrumbItem[] = [
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border p-4">
+                    <!-- <PlaceholderPattern /> -->
+                     <Switch />Switching
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                    <!-- <PlaceholderPattern /> -->
+                     <EmailNotifications />
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
